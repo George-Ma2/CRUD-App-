8fbb336033a3084b99072879e11d7e3bfed15550
George-Ma2 <george.ma@upr.edu>
2024-11-21 18:12:03 -0400

    Implemented login functionality
    implemented the necessary functionality for user login. This includes form handling, authentication, and redirecting users upon successful login.

--- CARE_Web_App/web_app/urls.py
+++ CARE_Web_App/web_app/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
 
     path('register', views.register, name='register'),
 
+    path('my-login', views.my_login, name='my-login'),
+
 ]
--- CARE_Web_App/web_app/views.py
+++ CARE_Web_App/web_app/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 
 from .forms import CreateUserForm, LoginForm
 
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate
+
 # Home Page
 def home(request):
 
@@ -24,4 +27,31 @@ def register(request):
     context = {'form': form}
 
     return render(request, 'web_app/register.html', context = context)
+
+# Login a user
+def my_login(request):
+
+    form = LoginForm()
+
+    if request.method == "POST":
+
+        form = LoginForm(request, data=request.POST)
+
+        if form.is_valid():
+
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+
+                auth.login(request, user)
+
+                return redirect('my-login')
+
+    context = {'form2':form}
+
+    return render(request, 'web_app/my-login.html', context=context)
+
 
