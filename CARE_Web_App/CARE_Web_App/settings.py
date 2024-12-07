b0d637d5653e27d69aac0171aed9ddcac8172346
George-Ma2 <george.ma@upr.edu>
2024-12-07 12:30:15 -0400

    Update database config

--- CARE_Web_App/CARE_Web_App/settings.py
+++ CARE_Web_App/CARE_Web_App/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'CARE_Web_App.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'CAREWebAppDB',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST':'localhost'
     }
 }
 
